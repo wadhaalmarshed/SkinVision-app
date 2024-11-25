854e994406880803f03756b0479401a49ff9e343
wadhaalmarshed <53091038+wadhaalmarshed@users.noreply.github.com>
2024-11-25 15:53:36 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,12 @@
 import streamlit as st
+from PIL import Image
 
-# Inject custom CSS to change background color
-st.markdown("""
-    <style>
-        body {
-            background-color: #DAC2B1;  /* Light beige color */
-        }
-    </style>
-""", unsafe_allow_html=True)
+# Title of the app
+st.title("Streamlit Image Display Example")
 
-# Add content to your app
+# Load an image (local file)
+image_path = "https://ibb.co/jTCVKkB"
+image = Image.open(image_path)
 
-image_url = "https://ibb.co/jTCVKkB"
-st.image(image_url, caption="logo", width=300)
+# Display the image in the app
+st.image(image, caption="This is your image", use_column_width=True)
