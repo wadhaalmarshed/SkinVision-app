be43c30a58eeb8bb298cbe05b393c0f442885309
wadhaalmarshed <53091038+wadhaalmarshed@users.noreply.github.com>
2024-11-25 15:57:45 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title("Welcome to My App")
 
 # Display an image as soon as the app loads
 image_url = "https://ibb.co/jTCVKkB"  # Replace with your image URL
-st.image(image_url, caption="Your Image Caption", use_column_width=True)
+st.image(image_url, use_column_width=True)
 
 # Additional app content here
 st.write("This is the content that appears below the image.")
